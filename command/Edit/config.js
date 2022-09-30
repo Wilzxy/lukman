030110c0b571155cd29eac4a55a42d5d97081c0c
Wilzxy <114745574+Wilzxy@users.noreply.github.com>
2022-09-30 20:06:38 +0700

    Update config.js

--- command/Edit/config.js
+++ command/Edit/config.js
@@ -1,9 +1,9 @@
 const { modul } = require('../Edit/module');
 const { chalk, fs, proces } = modul;
 
-global.ownerNumber = ["6282399255216@s.whatsapp.net"]
-global.namabotnya = 'Cyber Is Real'
-global.namaownernya = 'Cyber Executed'
+global.ownerNumber = ["6288232367642@s.whatsapp.net"]
+global.namabotnya = 'WilzCyber Bot'
+global.namaownernya = 'Itsme Wilzz'
 global.packname = '\n\n\n\n\n\n\n\n\n\n© Cyber||+62🇲🇨\nI`m From Indonesia\n\n\n\n\n\n\n\n\n\n'
 global.author = '\n\n\n\n\n\n\n\n\n\nWa : 0823-9925-5216\nYT : Cyber S-C-D-B\n\n\n\n\n\n\n\n\n\n'
 global.sessionName = 'CyberExe'
@@ -15,9 +15,9 @@ global.group = 'https://bit.ly/38oNU0D'
 global.youtube = 'https://bit.ly/39Ivus6'
 global.website = 'https://bit.ly/3yk3gOo'
 global.github = 'https://bit.ly/3FqVBj8'
-global.noown = 'https://wa.me/6282399255216'
+global.noown = 'https://wa.me/6288232367642'
 global.region = 'I`m From Indonesia'
-global.prefa = ['','!','.','#','-','�?']
+global.prefa = ['','!','.','#','-','�?']
 global.api = 
 {
     success: '```Success✅```',
